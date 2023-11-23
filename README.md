8d2a86dd87504301b212086d273ab50b9a2f3645
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-11-23 18:58:53 +0400

    Improve layout, add more parameters

---
+++ README.md
@@ -0,0 +1,6 @@
+# gpuhunt demo
+
+[**Try it now!**](https://gpuhunt.streamlit.app)
+
+* [dstackai/gpuhunt](https://github.com/dstackai/gpuhunt)
+* [dstackai/dstack](https://github.com/dstackai/dstack)
--- main.py
+++ main.py
@@ -2,14 +2,108 @@ import dataclasses
 from typing import List, Tuple
 
 import gpuhunt
+import gpuhunt.providers.vastai
 import pandas as pd
 import streamlit as st
 
 
-"""
-# `gpuhunt`
+st.set_page_config(
+    page_title="gpuhunt | Find the cheapest GPU",
+    layout="wide",
+    menu_items={
+        "Get help": "https://github.com/dstackai/gpuhunt/issues",
+        "Report a bug": "https://github.com/dstackai/gpuhunt-demo/issues",
+        "About": "https://github.com/dstackai/gpuhunt",
+    },
+)
 
-Visit [dstackai/gpuhunt](https://github.com/dstackai/gpuhunt) at GitHub
+
+@st.cache_data
+def get_catalog() -> gpuhunt.Catalog:
+    catalog = gpuhunt.Catalog(fill_missing=False, auto_reload=True)
+    catalog.load()
+    catalog.add_provider(gpuhunt.providers.vastai.VastAIProvider())
+    return catalog
+
+
+@st.cache_data
+def get_all_offers() -> List[gpuhunt.CatalogItem]:
+    return get_catalog().query(provider=PROVIDERS)
+
+
+PROVIDERS = ["aws", "azure", "datacrunch", "gcp", "nebius", "lambdalabs", "vastai"]
+ALL_OFFERS = get_all_offers()
+ALL_GPU_NAME = sorted(set(i.gpu_name for i in ALL_OFFERS if i.gpu_count > 0))
+ALL_GPU_MEM = [0.0] + sorted(set(i.gpu_memory for i in ALL_OFFERS if i.gpu_count > 0))
+ALL_GPU_COUNT = sorted(set(i.gpu_count for i in ALL_OFFERS))
+ALL_CPU = sorted(set(i.cpu for i in ALL_OFFERS))
+ALL_MEMORY = sorted(set(i.memory for i in ALL_OFFERS))
+
+DEFAULT_PROVIDERS = [p for p in PROVIDERS if p not in ["vastai"]]
+DEFAULT_GPUS = ["H100", "A100", "A6000", "A10", "A10G", "L40", "L4", "T4", "V100", "P100"]
+
+
+def format_version(v: str) -> str:
+    return f"{v[:4]}/{v[4:6]}/{v[6:]}"
+
+
+f"""
+# `gpuhunt`: find the cheapest GPU :dna:
+
+Catalog version: `{format_version(get_catalog().get_latest_version())}`
+"""
+
+with st.sidebar:
+    """## Configuration"""
+    providers = st.multiselect("Provider", options=PROVIDERS, default=DEFAULT_PROVIDERS)
+    gpu_count = st.select_slider("GPU count", options=ALL_GPU_COUNT, value=(ALL_GPU_COUNT[0], ALL_GPU_COUNT[-1]))
+    gpu_memory = st.select_slider("GPU memory", options=ALL_GPU_MEM, value=(ALL_GPU_MEM[0], ALL_GPU_MEM[-1]))
+    gpu_name = st.multiselect("GPU name", options=ALL_GPU_NAME, default=DEFAULT_GPUS)
+    spot = st.radio("Spot", options=["on-demand", "interruptable", "any"], index=0)
+    cpu = st.select_slider("CPU", options=ALL_CPU, value=(ALL_CPU[0], ALL_CPU[-1]))
+    memory = st.select_slider("RAM", options=ALL_MEMORY, value=(ALL_MEMORY[0], ALL_MEMORY[-1]))
+
+
+@st.cache_data
+def get_offers(
+        providers: List[str],
+        gpu_count: Tuple[int,
+        int],
+        gpu_memory: List[float],
+        gpu_name: List[str],
+        spot: str,
+        cpu: Tuple[int],
+        memory: Tuple[float],
+) -> pd.DataFrame:
+    offers = get_catalog().query(
+        provider=providers or None,
+        min_gpu_count=gpu_count[0], max_gpu_count=gpu_count[1],
+        min_gpu_memory=gpu_memory[0], max_gpu_memory=gpu_memory[-1],
+        min_cpu=cpu[0], max_cpu=cpu[-1],
+        min_memory=memory[0], max_memory=memory[-1],
+        gpu_name=gpu_name or None,
+        spot={"interruptable": True, "on-demand": False, "any": None}[spot],
+    )
+    df = pd.DataFrame([dataclasses.asdict(i) for i in offers])
+    if df.empty:
+        return df
+    df["gpu"] = df.apply(lambda row: "" if row.gpu_count == 0 else f"{row.gpu_name} ({row.gpu_memory:g} GB)", axis=1)
+    df = df[["provider", "price", "gpu_count", "gpu", "cpu", "memory", "spot", "location", "instance_name"]]
+    return df
+
+
+st.dataframe(
+    get_offers(providers, gpu_count, gpu_memory, gpu_name, spot, cpu, memory),
+    column_config={
+        "price": st.column_config.NumberColumn(format="$%.3f"),
+    },
+)
+
+
+"""
+Leave feedback at [dstackai/gpuhunt](https://github.com/dstackai/gpuhunt)
+
+## How it works
 
 ```bash
 pip install gpuhunt
@@ -17,44 +111,9 @@ pip install gpuhunt
 
 ```python
 >>> import gpuhunt
->>> offers = gpuhunt.query(min_gpu_count=1, min_gpu_memory=40, provider=["aws", "gcp"])
+>>> offers = gpuhunt.query(
+        min_gpu_count=1, min_gpu_memory=40,
+        provider=["aws", "gcp"]
+    )
 ```
 """
-
-
-@st.cache_data
-def get_catalog() -> gpuhunt.Catalog:
-    catalog = gpuhunt.Catalog(fill_missing=False, auto_reload=True)
-    catalog.load()
-    return catalog
-
-
-@st.cache_data
-def get_offers(providers: List[str], gpu_count: Tuple[int, int], gpu_memory: List[float], gpu_name: List[str], spot: List[str]) -> pd.DataFrame:
-    offers = get_catalog().query(
-        provider=providers or None,
-        min_gpu_count=gpu_count[0], max_gpu_count=gpu_count[1],
-        min_gpu_memory=gpu_memory[0], max_gpu_memory=gpu_memory[-1],
-        gpu_name=gpu_name or None,
-        spot=True if spot == ["yes"] else (False if spot == ["no"] else None),
-    )
-    return pd.DataFrame([dataclasses.asdict(i) for i in offers])
-
-
-PROVIDERS = ["aws", "azure", "gcp", "nebius", "lambdalabs"]
-ALL_GPUS = sorted(set(i.gpu_name for i in get_catalog().query(provider=PROVIDERS, min_gpu_count=1)))
-ALL_GPU_MEM = [0.0] + sorted(set(i.gpu_memory for i in get_catalog().query(provider=PROVIDERS, min_gpu_count=1)))
-ALL_GPU_COUNT = sorted(set(i.gpu_count for i in get_catalog().query(provider=PROVIDERS)))
-
-
-"""
-## Find the cheapest GPU
-"""
-
-providers = st.multiselect("Provider", options=PROVIDERS, default=PROVIDERS)
-gpu_count = st.select_slider("GPU count", options=ALL_GPU_COUNT, value=(ALL_GPU_COUNT[0], ALL_GPU_COUNT[-1]))
-gpu_memory = st.select_slider("GPU memory", options=ALL_GPU_MEM, value=(ALL_GPU_MEM[0], ALL_GPU_MEM[-1]))
-gpu_name = st.multiselect("GPU name", options=ALL_GPUS, default=ALL_GPUS)
-spot = st.multiselect("Spot", options=["yes", "no"], default=["yes", "no"])
-
-st.dataframe(get_offers(providers, gpu_count, gpu_memory, gpu_name, spot))
